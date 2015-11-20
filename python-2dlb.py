eaf4e5871ada58061116618f356e6a4181e8d7f3
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-11-19 20:10:28 -0500

    Creating a class based on Sauro's code.

--- python-2dlb.py
+++ python-2dlb.py
@@ -1,94 +1,179 @@
 import numpy as np
-import numpy.linalg as lingal
 import matplotlib
 matplotlib.use('TKagg')
-import matplotlib.pyplot as plt
-import math
-import skimage as ski
-import skimage.io
-from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+sns.set_context('poster', font_scale=1.25)
 
-
-# parameters
-lx=50  # length of domain in the x direction
-ly=20  # length of domain in the y direction
-w=np.array([4.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,1.0/36.0,
-    1.0/36.0,1.0/36.0,1.0/36.0]) # weights for directions
+##########################
+##### D2Q9 parameters ####
+##########################
+w=np.array([4./9.,1./9.,1./9.,1./9.,1./9.,1./36.,
+            1./36.,1./36.,1./36.]) # weights for directions
 cx=np.array([0,1,0,-1,0,1,-1,-1,1]) # direction vector for the x direction
 cy=np.array([0,0,1,0,-1,1,1,-1,-1]) # direction vector for the y direction
 tau=1
-g=10**-4
-cs=1/math.sqrt(3)
-timestep=1000 # time steps for the simulation
-timeplot=1 # plotimeing interval
+cs=1/np.sqrt(3)
+cs2 = cs**2
+cs22 = 2*cs2
+cssq = 2.0/9.0
 
-# initializing variables
-rho=np.ones((lx,ly)) # initializing the rho
-u=np.zeros((lx,ly)) # initializing the horizontal velocities
-v=np.zeros((lx,ly)) # initializing the vertical velocities
-f=np.zeros((9,lx,ly)) # initializing f
-conn=[0,3,4,1,2,7,8,5,6] #index of respective connectors
+w0 = 4./9.
+w1 = 1./9.
+w2 = 1./36.
 
-#this initializes the f function to be the weight ttimes the initial desnity
-for j in range(9):
-        f[j,:,:]=w[j]*rho
-ff=f
-#beging time incrementation loop, all of the code goes into this loop and analysis
-#is done over time incrementation
-for time in range(timestep):
-    #in this part we define the macroscopic behaviour, where the physics dictates how
-    #the particle distribution is migrated over the time incrementation
-    for i in range(lx):
-        for k in range(ly):
-            rho[i,k]=0
-            
-            for j in range(9):
-                rho[i,k]=rho[i,k]+f[j,i,k]
-                u[i,k]=u[i,k]+f[j,i,k]*cx[j]
-                v[i,k]=v[i,k]+f[j,i,k]*cy[j]
-                
-            u[i,k]=u[i,k]/rho[i,k]
-            v[i,k]=v[i,k]/rho[i,k]
+NUM_JUMPERS = 9
 
-    #here we define the collision array and is related as described in the problem
-    #statement from above
-    for i in range(lx):
-        for k in range(ly):
-            u[i,k]=u[i,k]+g*tau/rho[i,k]
-            for j in range(9):
-                cc=(1/cs**2)*(cx[j]*u[i,k]+cy[j]*v[i,k])
-                feq=rho[i,k]*w[j]*(1.0+cc+cc**2+v[i,k]**2)/(2*cs**2)
-                f[j,i,k]=f[j,i,k]+(1/tau)*(feq-f[j,i,k])
+class LB_Pipe_Flow(object):
+    """2d pipe flow with D2Q9"""
 
-    #rendefines for the next time step
-    ff=f
-    #boundary conditions
-    #north periodicity
-    for i in range(lx):
-        for j in range(9):
-            ff[j,i,ly-1]=f[conn[j],i,ly-1]
-            ff[j,i,0]=f[conn[j],i,0]
+    def __init__(self, tau=1., lx=400., ly=400.):
+        ### User input parameters
+        self.tau = tau
+        self.lx = lx # Grid not including boundary in x
+        self.ly = ly # Grid not including boundary in y
 
-	# in this loop we move the particles and essentially do the equivalent of a 
-	# circshift in MATLAB
-    for j in range(9):
-        f[j,:,:]=np.roll(np.roll(ff[j,:,:],cx[j],axis=0),cy[j],axis=1)
-        
-	#this part is for the plotting and only takes a few plot instances over time
-    plt.figure()
-    if not time%timeplot:
-        #print the current time step of the simulation
-        print "Time ",time," of ",timestep," completed" 
-        u_plot=np.array(np.transpose(u))
-        allx=np.array(range(lx+1))
-        ally=np.array(range(ly+1))
-        
-        #create the 2D color plot
-        fig, ax = plt.subplots()
-        plt.xlabel(r'x'); plt.ylabel(r'y')
-        heatmap = ax.pcolor(allx, ally, u_plot,vmin=0,vmax=0.0003)
-        colorbar = plt.colorbar(heatmap)
-        colorbar.set_label(r'value of velocity')
-        plt.gcf().set_size_inches(17, 7)
-        plt.draw()
-        plt.pause(0.01)
+        ## Everything else
+        self.nx = self.lx + 1 # Total size of grid in x including boundary
+        self.ny = self.ly + 1 # Total size of grid in y including boundary
+
+        self.viscosity = cs2*(tau-0.5)
+        self.omega = 1./self.tau
+
+        ## Initialize hydrodynamic variables
+        self.init_hydro()
+
+        # Intitialize the underlying probablistic fields
+        self.f=np.zeros((NUM_JUMPERS, self.nx, self.ny)) # initializing f
+        self.feq = np.zeros((NUM_JUMPERS, self.nx, self.ny))
+
+        self.update_feq()
+        self.init_pop()
+
+    def init_hydro(self):
+        nx = self.nx
+        ny = self.ny
+
+        self.rho = np.ones((nx, ny))
+        self.u_applied=cs/100
+        self.u = self.u_applied*(np.ones((nx, ny)) + np.random.randn(nx, ny))
+        self.v = (self.u_applied/100.)*(np.ones((nx, ny)) + np.random.randn(nx, ny))
+
+
+    def update_feq(self):
+        """Taken from sauro succi's code. This will be super easy to put on the GPU."""
+
+        u = self.u
+        v = self.v
+        feq = self.feq
+
+        ul = u/cs2
+        vl = v/cs2
+        uv = ul*vl
+        usq = u*u
+        vsq = v*v
+        sumsq  = (usq+vsq)/cs22
+        sumsq2 = sumsq*(1.-cs2)/cs2
+        u2 = usq/cssq
+        v2 = vsq/cssq
+
+        feq[0, :, :] = w0*(1. - sumsq)
+        feq[1, :, :] = w1*(1. - sumsq  + u2 + ul)
+        feq[2, :, :] = w1*(1. - sumsq  + v2 + vl)
+        feq[3, :, :] = w1*(1. - sumsq  + u2 - ul)
+        feq[4, :, :] = w1*(1. - sumsq  + v2 - vl)
+        feq[5, :, :] = w2*(1. + sumsq2 + ul + vl + uv)
+        feq[6, :, :] = w2*(1. + sumsq2 - ul + vl - uv)
+        feq[7, :, :] = w2*(1. + sumsq2 - ul - vl + uv)
+        feq[8, :, :] = w2*(1. + sumsq2 + ul - vl - uv)
+
+    def update_hydro(self):
+        f = self.f
+
+        self.rho = np.sum(f, axis=0)
+        inverse_rho = 1./self.rho
+
+        self.u = (f[1]-f[3]+f[5]-f[6]-f[7]+f[8])*inverse_rho
+        self.v = (f[5]+f[2]+f[6]-f[7]-f[4]-f[8])*inverse_rho
+
+
+    def move_bcs(self):
+        f = self.f
+        lx = self.lx
+        ly = self.ly
+
+        # West inlet: periodic BC's
+        for j in range(1,ly):
+            f[1,0,j] = f[1,lx,j]
+            f[5,0,j] = f[5,lx,j]
+            f[8,0,j] = f[8,lx,j]
+        # EAST outlet
+        for j in range(1,ly):
+            f[3,lx,j] = f[3,0,j]
+            f[6,lx,j] = f[6,0,j]
+            f[7,lx,j] = f[7,0,j]
+        # NORTH solid
+        for i in range(1, lx): # Bounce back
+            f[4,i,ly] = f[2,i,ly-1]
+            f[8,i,ly] = f[6,i+1,ly-1]
+            f[7,i,ly] = f[5,i-1,ly-1]
+        # SOUTH solid
+        for i in range(1, lx):
+            f[2,i,0] = f[4,i,1]
+            f[6,i,0] = f[8,i-1,1]
+            f[5,i,0] = f[7,i+1,1]
+
+        # Corners bounce-back
+        f[8,0,ly] = f[6,1,ly-1]
+        f[5,0,0]  = f[7,1,1]
+        f[7,lx,ly] = f[5,lx-1,ly-1]
+        f[6,lx,0]  = f[8,lx-1,1]
+
+    def move(self):
+        f = self.f
+        lx = self.lx
+        ly = self.ly
+
+        for j in range(ly,0,-1): # Up, up-left
+            for i in range(0, lx):
+                f[2,i,j] = f[2,i,j-1]
+                f[6,i,j] = f[6,i+1,j-1]
+        for j in range(ly,0,-1): # Right, up-right
+            for i in range(lx,0,-1):
+                f[1,i,j] = f[1,i-1,j]
+                f[5,i,j] = f[5,i-1,j-1]
+        for j in range(0,ly): # Down, right-down
+            for i in range(lx,0,-1):
+                f[4,i,j] = f[4,i,j+1]
+                f[8,i,j] = f[8,i-1,j+1]
+        for j in range(0,ly): # Left, left-down
+            for i in range(0, lx):
+                f[3,i,j] = f[3,i+1,j]
+                f[7,i,j] = f[7,i+1,j+1]
+
+    def init_pop(self):
+        f = self.f
+        feq = self.feq
+        nx = self.nx
+        ny = self.ny
+
+        f = feq.copy()
+        # We now slightly perturb f
+        amplitude = .01
+        perturb = (1. + amplitude*np.random.randn(nx, ny))
+        f *= perturb
+
+    def collide_particles(self):
+        f = self.f
+        feq = self.feq
+        omega = self.omega
+
+        self.f = f*(1.-omega)+omega*feq
+
+    def run(self, num_iterations):
+        for cur_iteration in range(num_iterations):
+            self.move_bcs() # We have to udpate the boundary conditions first, or we are in trouble
+            self.move() # Move all jumpers
+            self.update_hydro() # Update the hydrodynamic variables
+            self.update_feq() # Update the equilibrium fields
+            self.collide_particles() # Relax the nonequilibrium fields
+
