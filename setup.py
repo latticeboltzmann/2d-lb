44048b1a2b811b127bcd35b00171488e9626bca1
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2015-11-29 13:56:02 -0500

    getting it to run on Matts computer

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import cython_gsl
+# import cython_gsl
 import numpy as np
 
 extensions = [
