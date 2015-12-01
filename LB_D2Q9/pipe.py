dad364cef52edfe6dc89eb59885cd9b36aa88fd0
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2015-11-30 20:28:44 -0500

    got periodic boundary conditions to work on the python code! :)

--- LB_D2Q9/pipe.py
+++ LB_D2Q9/pipe.py
@@ -250,6 +250,74 @@ class Pipe_Flow(object):
             self.collide_particles() # Relax the nonequilibrium fields
 
 
+class Pipe_Flow_PeriodicBC(Pipe_Flow):
+
+    def __init__(self, **kwargs):
+        super(Pipe_Flow_PeriodicBC, self).__init__(**kwargs)
+
+    def move_bcs(self):
+        """This is slow; cythonizing makes it fast."""
+
+        lx = self.lx
+        ly = self.ly
+
+        farr = self.f
+
+        # INLET: constant pressure!
+        farr[1, 0, 1:ly] = farr[3, 0, 1:ly] + (2./3.)*self.inlet_rho*self.u[0, 1:ly]
+        farr[5, 0, 1:ly] = -.5*farr[2,0,1:ly]+.5*farr[4, 0, 1:ly]+farr[7, 0, 1:ly] + (1./6.)*self.u[0, 1:ly]*self.inlet_rho
+        farr[8, 0, 1:ly] = .5*farr[2,0,1:ly]-.5*farr[4, 0, 1:ly]+farr[6, 0, 1:ly] + (1./6.)*self.u[0, 1:ly]*self.inlet_rho
+
+        # OUTLET: constant pressure!
+        farr[3, lx, 1:ly] = farr[1, lx, 1:ly] - (2./3.)*self.outlet_rho*self.u[lx,1:ly]
+        farr[6, lx, 1:ly] = -.5*farr[2,lx,1:ly]+.5*farr[4,lx,1:ly]+farr[8,lx,1:ly]-(1./6.)*self.u[lx,1:ly]*self.outlet_rho
+        farr[7, lx, 1:ly] = .5*farr[2,lx,1:ly]-.5*farr[4,lx,1:ly]+farr[5,lx,1:ly]-(1./6.)*self.u[lx,1:ly]*self.outlet_rho
+
+        f = self.f
+        inlet_rho = self.inlet_rho
+        outlet_rho = self.outlet_rho
+
+        # NORTH periodic
+        for i in range(1, lx): # update the values of f at the top with those from the bottom
+            f[4,i,ly] = f[4,i,0]
+            f[8,i,ly] = f[8,i,0]
+            f[7,i,ly] = f[7,i,0]
+        # SOUTH periodic
+        for i in range(1, lx): #update the values of f at the bottom with those from the top
+            f[2,i,0] = f[2,i,ly]
+            f[6,i,0] = f[6,i,ly]
+            f[5,i,0] = f[5,i,ly]
+
+        ### Corner nodes: Tricky & a huge pain ###
+        # BOTTOM INLET
+        f[1, 0, 0] = f[1, 0, ly]
+        f[2, 0, 0] = f[2, 0, ly]
+        f[5, 0, 0] = f[5, 0, ly]
+        f[6, 0, 0] = .5*(-f[0,0,0]-2*f[3,0,0]-2*f[4,0,0]-2*f[7,0,0]+inlet_rho)
+        f[8, 0, 0] = .5*(-f[0,0,0]-2*f[3,0,0]-2*f[4,0,0]-2*f[7,0,0]+inlet_rho)
+
+        # TOP INLET
+        f[1, 0, ly] = f[1, 0, 0]
+        f[4, 0, ly] = f[4, 0, 0]
+        f[8, 0, ly] = f[8, 0, 0]
+        f[5, 0, ly] = .5*(-f[0,0,ly]-2*f[2,0,ly]-2*f[3,0,ly]-2*f[6,0,ly]+inlet_rho)
+        f[7, 0, ly] = .5*(-f[0,0,ly]-2*f[2,0,ly]-2*f[3,0,ly]-2*f[6,0,ly]+inlet_rho)
+
+        # BOTTOM OUTLET
+        f[3, lx, 0] = f[1, lx, 0]
+        f[2, lx, 0] = f[4, lx, 0]
+        f[6, lx, 0] = f[8, lx, 0]
+        f[5, lx, 0] = .5*(-f[0,lx,0]-2*f[1,lx,0]-2*f[4,lx,0]-2*f[8,lx,0]+outlet_rho)
+        f[8, lx, 0] = .5*(-f[0,lx,0]-2*f[1,lx,0]-2*f[4,lx,0]-2*f[8,lx,0]+outlet_rho)
+
+        # TOP OUTLET
+        f[3, lx, ly] = f[1, lx, ly]
+        f[4, lx, ly] = f[2, lx, ly]
+        f[7, lx, ly] = f[5, lx, ly]
+        f[6, lx, ly] = .5*(-f[0,lx,ly]-2*f[1,ly,ly]-2*f[2,lx,ly]-2*f[5,lx,ly]+outlet_rho)
+        f[8, lx, ly] = .5*(-f[0,lx,ly]-2*f[1,ly,ly]-2*f[2,lx,ly]-2*f[5,lx,ly]+outlet_rho)        
+
+
 class Pipe_Flow_Obstacles(Pipe_Flow):
 
     def __init__(self, obstacle_mask=None, **kwargs):
