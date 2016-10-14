Fixed a couple of factors when taking the FFT to get the x & y gradients.

LB_D2Q9/spectral_poisson/screened_poisson.py
@@ -62,8 +62,8 @@ class Screened_Poisson(object):
         self.xgrad_transform = gfft.fft.FFT(self.context, self.queue, (self.xgrad,), axes=(0, 1))
         self.ygrad_transform = gfft.fft.FFT(self.context, self.queue, (self.ygrad,), axes=(0, 1))
 
-        self.xgrad_rescale = np.pi*1.0j*self.freq_X
-        self.ygrad_rescale = np.pi * 1.0j * self.freq_Y
+        self.xgrad_rescale = 2*np.pi * 1.0j*self.freq_X
+        self.ygrad_rescale = 2*np.pi * 1.0j * self.freq_Y
 
 
     def update_grad_fields(self):